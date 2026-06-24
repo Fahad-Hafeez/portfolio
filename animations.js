210f5c91f2701bd2c643290c0a5e175b649eb9bd
Fahad Hafeez <fahadhafeez650@gmail.com>
2026-06-24 17:36:01 +0100

    Add Intersection Observer for animations

---
+++ animations.js
@@ -0,0 +1,25 @@
+
+const observer=new IntersectionObserver(entries=>{
+
+
+entries.forEach(entry=>{
+
+
+if(entry.isIntersecting){
+
+
+entry.target.classList.add("show");
+
+
+}
+
+
+})
+
+
+})
+
+
+document.querySelectorAll(".hidden")
+
+.forEach(el=>observer.observe(el))
