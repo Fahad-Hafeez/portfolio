c75030cbf51760058c8a740847aac377eab1ca7e
Fahad Hafeez <fahadhafeez650@gmail.com>
2026-06-26 15:42:49 +0000

    updates

--- js/counters.js
+++ js/counters.js
@@ -34,9 +34,9 @@ if (metricsEl) {
     (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          animateCounter('m-pubs',  17);
-          animateCounter('m-data',  8);
-          animateCounter('m-proj',  25);
+          animateCounter('m-pubs',  7);
+          animateCounter('m-data',  2);
+          animateCounter('m-proj',  6);
           animateCounter('m-stars', 1400, 2000);
           counterObserver.unobserve(entry.target);
         }
